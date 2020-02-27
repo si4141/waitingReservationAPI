5249e769606eee5c3bfe776dc2856252bdb6811b
si4141 <ced4141@me.com>
2020-02-28 00:45:37 +0900

    Add STATIC_ROOT

--- waiting_notifier/waiting_notifier/settings.py
+++ waiting_notifier/waiting_notifier/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 load_dotenv(os.path.join(BASE_DIR, '.env'))
 LINE_ACCESS_TOKEN = os.environ.get('LINE_ACCESS_TOKEN')
