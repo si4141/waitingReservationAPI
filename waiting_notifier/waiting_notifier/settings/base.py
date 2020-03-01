2f8a9a0335fb54d18b3e7d7d15c9c3688b3aec2a
si4141 <ced4141@me.com>
2020-03-01 19:18:21 +0900

    Add django-heroku

--- waiting_notifier/waiting_notifier/settings/base.py
+++ waiting_notifier/waiting_notifier/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from dotenv import load_dotenv
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -119,3 +120,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 load_dotenv(os.path.join(BASE_DIR, '.env'))
 LINE_ACCESS_TOKEN = os.environ.get('LINE_ACCESS_TOKEN')
+django_heroku.settings(locals())
