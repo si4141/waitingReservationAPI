cfd87efe6da1a4c3bb3704c4d9c0f27b728c0358
si4141 <ced4141@me.com>
2020-03-03 02:18:35 +0900

    add cors middware, temporary allow all origin

--- waiting_notifier/waiting_notifier/settings/base.py
+++ waiting_notifier/waiting_notifier/settings/base.py
@@ -34,7 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app.apps.AppConfig',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -45,7 +46,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'waiting_notifier.urls'
 
