2e94ad4e1e14c166644e981f836da5be6b9c3e65
si4141 <ced4141@me.com>
2020-03-03 01:33:49 +0900

    Add fixture

--- waiting_notifier/app/admin.py
+++ waiting_notifier/app/admin.py
@@ -19,18 +19,3 @@ class ReservationSlotAdmin(admin.ModelAdmin):
 admin.site.register(User, UserAdmin)
 admin.site.register(ReservationSlot, ReservationSlotAdmin)
 admin.site.register(Reservation)
-
-
-if not ReservationSlot.objects.exists():
-    import datetime
-    jst = datetime.timezone(datetime.timedelta(hours=9), 'JST')
-    start_times = []
-    start = datetime.datetime(2020, 4, 25, 10, 00, tzinfo=jst)
-    while start < datetime.datetime(2020, 4, 26, 17, 0, tzinfo=jst):
-        if 10 <= start.hour < 17:
-            start_times.append(start)
-        start += datetime.timedelta(minutes=10)
-
-    for start in start_times:
-        rs = ReservationSlot(start_time=start)
-        rs.save()
